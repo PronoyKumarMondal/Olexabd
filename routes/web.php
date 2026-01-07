736841d854471912403c5526cc5d312e5130070b
PronoyKumarMondal <pronoy15-14744@diu.edu.bd>
2026-01-08 02:07:26 +0600

    Security: Add AdminMiddleware to block customers from /admin

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/account/orders/{order}', [OrderController::class, 'show'])->name('orders.show');
 
     // Admin Routes
-    Route::prefix('admin')->name('admin.')->middleware(['auth'])->group(function () {
+    Route::prefix('admin')->name('admin.')->middleware(['auth', 'admin'])->group(function () {
         Route::get('/', [\App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
         
         Route::resource('categories', \App\Http\Controllers\Admin\CategoryController::class);
