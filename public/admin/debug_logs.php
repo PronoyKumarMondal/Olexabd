c085a3495cf95bdd647f1211dfe5b6a6da46923b
PronoyKumarMondal <pronoy15-14744@diu.edu.bd>
2026-01-08 03:01:56 +0600

    Fix Debug Log Path: Go up 2 levels to find storage directory

--- public/admin/debug_logs.php
+++ public/admin/debug_logs.php
@@ -2,7 +2,7 @@
 // Security: Only allow if a specific simple secret is passed or IP matches (skipping for now as it's temporary)
 // Read the last 100 lines of the Laravel log file
 
-$logFile = __DIR__ . '/../storage/logs/laravel.log';
+$logFile = __DIR__ . '/../../storage/logs/laravel.log';
 
 if (!file_exists($logFile)) {
     die("Log file not found at: " . $logFile);
