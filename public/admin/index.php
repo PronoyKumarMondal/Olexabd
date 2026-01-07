a6c6a0dbfb16ac5e54c2ad7d3c6e2eaaf7e31837
PronoyKumarMondal <pronoy15-14744@diu.edu.bd>
2026-01-08 02:35:19 +0600

    Fix Admin Subdomain: Add proxy index.php in public/admin

---
+++ public/admin/index.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Adjust paths to point to the main project root (Go up 2 levels: public/admin -> root)
+if (file_exists($maintenance = __DIR__.'/../../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register availability of the Composer autoloader...
+require __DIR__.'/../../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__.'/../../bootstrap/app.php')
+    ->handleRequest(Request::capture());
