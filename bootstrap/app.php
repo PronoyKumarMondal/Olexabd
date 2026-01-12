cd7ec5ef5484afa92732fe4abb22585bc6243082
PronoyKumarMondal <pronoy15-14744@diu.edu.bd>
2026-01-13 03:13:33 +0600

    Config: Register TrackSource middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
             \App\Http\Middleware\LogRequestActivity::class,
+            \App\Http\Middleware\TrackSource::class,
         ]);
         
         $middleware->alias([
