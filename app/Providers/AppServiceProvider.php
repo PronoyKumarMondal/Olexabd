c41cea1230e789c5128e11c46c692cf64f95d79b
PronoyKumarMondal <pronoy15-14744@diu.edu.bd>
2026-01-27 00:12:10 +0600

    Fix: Force HTTPS Scheme to prevent 419 Errors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         \Illuminate\Support\Facades\Schema::defaultStringLength(191);
         \Illuminate\Pagination\Paginator::useBootstrapFive();
 
